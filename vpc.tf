9d2e2e9f3e5cc00e133b55755ffa97f1368795b4
lav990 <lavanyakakara.1997@gmail.com>
2025-09-20 21:51:15 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "tier-3-vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "tier-3-vpc"
+  }
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public-web-subnet" {
+  vpc_id     = aws_vpc.tier-3-vpc.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name = "public-web-subnet"
+  }
+}
+
+variable "subnet_cidr" {
+  default = "10.0.0.0/24"
+}
+
+output "subnet-public-id" {
+  value = aws_subnet.public-web-subnet.id
+}
